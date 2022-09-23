70fb015e536862b501d36edfb3187ed5f1bd583e
bushido117 <vadim.saiko53370@mail.ru>
2022-09-23 15:44:22 +0300

    Initial Commit

---
+++ SideMenuHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuHW
+//
+//  Created by Вадим Сайко on 23.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
